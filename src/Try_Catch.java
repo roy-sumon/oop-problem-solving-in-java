69b2e24b30511c82f1103c78e83fcf9708dfb198
sumon <roysumonbd90@gmail.com>
2024-05-24 17:20:59 +0600

    Multiple Exception handle

--- src/Try_Catch.java
+++ src/Try_Catch.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Try_Catch {
     public static void main(String[] args) {
@@ -6,10 +6,16 @@ public class Try_Catch {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = 0;
+        int[] arr = {1,2,3,4,5};
         try {
             c = a/b;
-        }catch (ArithmeticException e){
+            arr[6] = 8;
+        }
+        catch (ArithmeticException e){
             System.out.println("can not " + e.getMessage());
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
         }
         System.out.println(c);
 
